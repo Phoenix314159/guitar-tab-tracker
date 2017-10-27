16fbbdf16123a25348aaf687eff1f1bc0e158904
James Langford <derivative7117@live.com>
2017-10-26 23:49:09 -0600

    updated config/prod

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,19 @@
 process.env.PWD = process.cwd();
 
 module.exports = {
-  dirname: process.env.PWD
+  dirname: process.env.PWD,
+  port: process.env.PORT,
+  cookie: {
+    cookieKey: process.env.COOKIE_KEY,
+    maxAge: process.env,MAX_AGE
+  },
+  dbConnection: {
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
+    max: process.env.POOL_MAX,
+    idleTimeoutMillis: process.env.TIME_OUT
+  }
 }
